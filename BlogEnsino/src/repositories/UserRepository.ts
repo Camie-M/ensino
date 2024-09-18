e140861b3d68121e24b25f5904e0439bdbe80fcb
breno.ferrari <breno.ferrari@fiap.com.br>
2024-09-18 00:50:53 -0300

    Passando entitys para users

--- BlogEnsino/src/repositories/UserRepository.ts
+++ BlogEnsino/src/repositories/UserRepository.ts
@@ -1,38 +1,49 @@
+import { UserEntity } from '../entities/UserEntity';
 import { User } from '../models/User';
 
 export class UserRepository {
-    async findAll() {
-        return User.findAll();
+    async findAll(): Promise<UserEntity[]> {
+        const users = await User.findAll();
+        return users.map(user => new UserEntity(user.username, user.role, user.id));
     }
 
-    async findById(id: string) {
-        return User.findByPk(id);
+    async findById(id: string): Promise<UserEntity | null> {
+        const user = await User.findByPk(id);
+        if (user) {
+            return new UserEntity(user.username, user.role, user.id);
+        }
+        return null;
     }
 
-    async create(username: string, role: string) {
-        return User.create({ username, role });
+    async create(username: string, role: string): Promise<UserEntity> {
+        const user = await User.create({ username, role });
+        return new UserEntity(user.username, user.role, user.id);
     }
 
-    async update(id: string, updatedFields: { username: string; role: string }) {
+
+    async update(id: string, updatedFields: { username: string; role: string }): Promise<UserEntity | null> {
         try {
             const user = await User.findByPk(id);
             if (user) {
-                return user.update(updatedFields);
+                const updatedUser = await user.update(updatedFields);
+                return new UserEntity(updatedUser.username, updatedUser.role, updatedUser.id);
             }
+            return null;
         } catch (error) {
-            throw new Error(`Não foi possivel atualizar o user ${error}`);
+            throw new Error(`Não foi possível atualizar o usuário: ${error}`);
         }
     }
 
-    async delete(id: string) {
+    async delete(id: string): Promise<UserEntity | null> {
         try {
             const user = await User.findByPk(id);
             if (user) {
-                user.destroy();
+                await user.destroy();
+                return new UserEntity(user.username, user.role, user.id);
             }
-            return user
+            return null;
         } catch (error) {
-            throw new Error(`Não foi deletar o user ${error}`);
+            throw new Error(`Não foi possível deletar o usuário: ${error}`);
         }
     }
 }
