cd23a4e6d687a1bd640ea20a21b9de627f0c4410
Camie-M <camilamouradev@gmail.com>
2024-09-23 21:13:27 -0300

    subindo mocks

--- EnsinoBackend/dist/routes/postRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { PostController } from '../controllers/PostController';
-const postRoutes = Router();
-postRoutes.post('/create-post', PostController.createPost);
-postRoutes.get('/get-posts', PostController.getAllPosts);
-postRoutes.get('/get-post/:id', PostController.getPostById);
-postRoutes.put('/edit-post/:id', PostController.editPost);
-postRoutes.delete('/delete-post/:id', PostController.deletePost);
-export default postRoutes;
