ccdc128a218cc7ccb403884557324e0c1cc52ce4
breno.ferrari <breno.ferrari@fiap.com.br>
2024-11-26 00:28:09 -0300

    Corrigindo erro de cord do servidor

--- BlogEnsino/src/server.ts
+++ BlogEnsino/src/server.ts
@@ -3,14 +3,24 @@ import router from './routes';
 import sequelize from './config/database';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from '../swagger_output.json';
-
+const cors = require('cors');
 const app = express();
 
+
+app.use(cors());
+// app.use(cors({
+//   origin: 'http://localhost:3000', // Permite apenas esta origem
+//   methods: 'GET, POST, PUT, DELETE', // Métodos permitidos
+//   allowedHeaders: 'Content-Type, Authorization', // Cabeçalhos permitidos
+// }));
+
 app.use(express.json());
-app.use(router);
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+app.use(router);
+
+// Sincronização do Sequelize com o banco de dados
 sequelize.sync({ alter: true }).then(() => {
   console.log('Models synchronized with the database');
 }).catch((error) => {
