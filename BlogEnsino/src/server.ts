1f2b20f794b670cdfe47c0da7ec67e0214feb6e4
breno.ferrari <breno.ferrari@fiap.com.br>
2024-11-27 00:24:14 -0300

    v1 para mockar dados no build

--- BlogEnsino/src/server.ts
+++ BlogEnsino/src/server.ts
@@ -1,8 +1,14 @@
+import { Post } from './models/Post';
+import { User } from './models/User';
 import express from 'express';
 import router from './routes';
 import sequelize from './config/database';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from '../swagger_output.json';
+import { v4 as uuidv4 } from 'uuid';
+import { UserService } from './services/UserService';
+import { UserRepository } from './repositories/UserRepository';
+
 const cors = require('cors');
 const app = express();
 
@@ -22,6 +28,8 @@ app.use(router);
 
 // Sincronização do Sequelize com o banco de dados
 sequelize.sync({ alter: true }).then(() => {
+  creatMockUser();
+  creatMockPost();
   console.log('Models synchronized with the database');
 }).catch((error) => {
   console.error('Error syncing models with the database', error);
@@ -30,3 +38,57 @@ sequelize.sync({ alter: true }).then(() => {
 app.listen(3001, () => {
   console.log('Servidor rodando na porta 3001');
 });
+
+const creatMockUser = async () => {
+  const User = sequelize.models.User
+  await User.bulkCreate([
+    {
+      username: "Breno",
+      role: "admin"
+    },
+    {
+      username: "Camila",
+      role: "user"
+    },
+    {
+      username: "Valdir",
+      role: "admin"
+    },
+    {
+      username: "Matheus",
+      role: "admin"
+    },
+
+  ])
+
+}
+const creatMockPost = async () => {
+  const Post = sequelize.models.Post
+  const userAdmin = await User.findOne({ where: { username: "Breno" } })
+  if (userAdmin) {
+    await Post.bulkCreate([
+      {
+        title: "titulo1",
+        text: "texto1",
+        user_id: userAdmin.id
+      },
+      {
+        title: "titulo2",
+        text: "texto2",
+        user_id: userAdmin.id
+      },
+      {
+        title: "titulo3",
+        text: "texto3",
+        user_id: userAdmin.id
+      },
+      {
+        title: "titulo4",
+        text: "texto4",
+        user_id: userAdmin.id
+      },
+    ])
+  }
+
+
+}
